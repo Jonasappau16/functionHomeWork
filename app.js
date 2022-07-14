c35b670587c0a74d2e40f162fdc5359c82471a20
J@y14 <jonasappau16@gmail.com>
2022-07-13 20:51:16 -0400

    function homework

---
+++ app.js
@@ -0,0 +1,46 @@
+/*In your app.js complete the following steps.
+
+1. Declare ten different variables and assign them the value of a number.
+2. Two times, console.log the addition of two of the variables you declared.
+3. Two times, console.log the subtraction of two of the variables you declared.
+4. Two times, console.log the multiplication of two of the variables you declared.
+5. Two times, console.log the division of two of the variables you declared.
+6. Two times, increments one of the variables you declared.
+7. Two times, decrement one of the variables you have declared.
+8. Deploy your project to github and submit a link to your repository for your homework.*/
+
+
+var a = 10;
+var b = 20;
+var c = 30;
+var e = 40;
+var f = 15;
+var g = 16;
+var h = 11;
+var i = 3;
+var j = 14;
+var k = 7;
+
+// addition 
+console.log (a + f);
+console.log(j + k);
+
+// subtraction
+console.log(b - i);
+console.log(f - g);
+
+// multiply
+console.log(h * e);
+console.log(a * f);
+
+// division
+console.log(c / a);
+console.log(e / g);
+
+
+// increment
+j++
+
+
+// decrement 
+a--
